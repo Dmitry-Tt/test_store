d8becc5084dac37cb2c0bb5574b09d332e10d974
Dmitry Tyukhtenev <94732692+Dmitry-Tt@users.noreply.github.com>
2026-04-13 20:45:06 +0700

    added flask run

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 :: Для запуска проекта нужно написать в консоли run.bat
-set FLASK_APP=webapp && set FLASK_ENV=development && set FLASK_DEBUG=1 && flask 
+set FLASK_APP=webapp && set FLASK_ENV=development && set FLASK_DEBUG=1 && flask run
